749dc75258c81df0e520a2e11e00411a902fa4e4
youngil <youngil@placeb.com>
2024-03-16 18:13:58 +0900

    버그 수정

--- JavaScript02/app.js
+++ JavaScript02/app.js
@@ -7,17 +7,21 @@ while(!maximum) {
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
 
-let guess = prompt("Enter your first guess!");
+let guess = prompt("Enter your first guess! (Type 'q' to quit)");
 let attempts = 1;
 
 while (parseInt(guess) !== targetNum) {
     if(guess === 'q') break; // added this line to allow user to quit the game by entering 'q'
-    attempts++;
+    guess = parseInt(guess);    
     if (guess > targetNum) {
         guess = prompt("Too high! Enter a new guess!");
-    } else {
+        attempts++;
+    } else if(guess < targetNum) {
         guess = prompt("Too low! Enter a new guess!");
-    }    
+        attempts++;
+    } else {
+        guess = prompt("Invalid guess! Enter a new guess! (Type 'q' to quit)");
+    }
 }
 
 if(guess === 'q') {
