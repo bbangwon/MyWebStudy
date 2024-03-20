d36930eef7048ddfa6e01ca81035ff5a83040adf
youngil <youngil@placeb.com>
2024-03-20 09:58:04 +0900

    map 메서드

--- JavaScript06/app.js
+++ JavaScript06/app.js
@@ -5,3 +5,9 @@ numbers.forEach(function (el) {
     if(el % 2 === 0)
         console.log(el);
 });
+
+//map() 함수는 배열의 각 요소에 대해 한 번씩 제공된 함수를 실행하고, 그 결과를 모아 새로운 배열을 반환합니다.
+const doubles = numbers.map(function (el) {
+    return el * 2;
+});
+console.log(doubles);
