d502955f6a5fd4a34238a1a571d9ea185cb019f9
youngil <youngil@placeb.com>
2024-03-20 09:56:48 +0900

    forEach

---
+++ JavaScript06/app.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// forEach() 함수는 배열의 각 요소에 대해 한 번씩 제공된 함수를 실행합니다.
+numbers.forEach(function (el) {
+    if(el % 2 === 0)
+        console.log(el);
+});
