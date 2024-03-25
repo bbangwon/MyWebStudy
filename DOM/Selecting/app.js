64460de0f7e1b8f9d5080acbe3fcddb7650a8be4
youngil <youngil@placeb.com>
2024-03-25 10:26:05 +0900

    querySelector, querySelectorAll

--- DOM/Selecting/app.js
+++ DOM/Selecting/app.js
@@ -1,5 +1,9 @@
 const allImages = document.getElementsByTagName('img');
-
 for (let img of allImages) {
     console.log(img.src);
+}
+
+const links = document.querySelectorAll('p a');
+for(let link of links) {
+    console.log(link.href);
 }
