e027e3b98eccd8e0cf8625271945924737f99cb6
youngil <youngil@placeb.com>
2024-03-26 14:12:30 +0900

    pokemon demo

--- DOM/Manipulating/app.js
+++ DOM/Manipulating/app.js
@@ -9,3 +9,16 @@ h2.classList.remove('border');
 
 // add/remove toggle
 h2.classList.toggle('border');
+
+const firstBold = document.querySelector('b');
+const paragraph = firstBold.parentElement;
+const ChildrenOfParagraph = paragraph.children;
+
+//Node vs Element
+const squareImg = document.querySelector('.square');
+const squareImgPrevious = squareImg.previousElementSibling;
+const squareImgNext = squareImg.nextElementSibling;
+
+const squareImgPreviousSibling = squareImg.previousSibling;
+const squareImgNextSibling = squareImg.nextSibling;
+
