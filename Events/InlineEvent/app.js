65f8b6f17357ec2e2957de38befbed0d201c089a
youngil <youngil@placeb.com>
2024-03-27 14:15:06 +0900

    addEventListener

--- Events/InlineEvent/app.js
+++ Events/InlineEvent/app.js
@@ -9,3 +9,29 @@ function scream(){
 }
 
 btn.onmouseenter = scream;
+
+const btn3 = document.querySelector('#v3');
+
+btn3.addEventListener('click', function() {
+    alert('Button Clicked');
+});
+
+
+function twist() {
+    console.log('twist');
+}
+
+function shout() {
+    console.log('shout');
+}
+
+const tasButton = document.querySelector('#tas');
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+// 콜백을 계속 추가
+tasButton.addEventListener('click', twist, { once: true });   // once: true로 설정하면 한번만 실행
+tasButton.addEventListener('click', shout);
+
+
