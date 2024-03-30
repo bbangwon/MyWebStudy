109886020bf9adba973e7d4983451a8922d48e90
정영일 <bbangwon@outlook.com>
2024-03-30 22:43:38 +0900

    preventDefault 연습

--- Events/formEvent/app.js
+++ Events/formEvent/app.js
@@ -1,7 +1,14 @@
 const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const list = document.querySelector('#cats');
 form.addEventListener('submit', function(evt) {    
     //기본 이벤트를 막는다.
     evt.preventDefault();  
     
-    console.log('Form Submitted');
+    console.log(input.value);
+
+    const li = document.createElement('li');
+    li.innerText = input.value;
+    list.appendChild(li);
+    input.value = '';
 });
