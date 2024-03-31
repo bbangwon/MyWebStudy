3b43ce5ce471c6b903441ff1f9532f8bb27c1793
정영일 <bbangwon@outlook.com>
2024-03-31 19:15:50 +0900

    ScoreKeeper - 2

--- ScoreKeeper/app.js
+++ ScoreKeeper/app.js
@@ -1,13 +1,15 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
 
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
 
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function() {
@@ -15,6 +17,8 @@ p1Button.addEventListener('click', function() {
         p1Score += 1;
         if(p1Score === winningScore){
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
         p1Display.innerText = p1Score;
     }  
@@ -25,7 +29,27 @@ p2Button.addEventListener('click', function() {
         p2Score += 1;
         if(p2Score === winningScore){
             isGameOver = true;
+            p1Display.classList.add('loser');
+            p2Display.classList.add('winner');
         }
         p2Display.innerText = p2Score;    
     }
 });
+
+winningScoreSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    reset();
+});
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.innerText = 0;
+    p2Display.innerText = 0;
+    
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');    
+}
