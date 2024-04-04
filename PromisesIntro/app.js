f99973bdb2661d1cc1a629d1830d25d7468c85bb
youngil <youngil@placeb.com>
2024-04-04 09:20:48 +0900

    프로미스 체이닝

--- PromisesIntro/app.js
+++ PromisesIntro/app.js
@@ -39,20 +39,34 @@ const fakeRequestPromise = (url) => {
 //     console.log('ERROR!!!', err);
 // });
 
-//프로미스 체이닝
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+//     .then(() => {        
+//         console.log('IT WORKED!!! (page1)');
+
+//         fakeRequestPromise('yelp.com/api/coffee/page2')
+//             .then(() => {        
+//                 console.log('IT WORKED!!! (page2)');
+//             })
+//             .catch(() => {
+//                 console.log('OH NO, A REQUEST ERROR!!! (page2)');
+//             });
+//     })
+//     .catch(() => {
+//         console.log('OH NO, A REQUEST ERROR!!! (page1)');
+//     });
+
+//프로미스
 fakeRequestPromise('yelp.com/api/coffee/page1')
-    .then(() => {        
+    .then(data => {        
         console.log('IT WORKED!!! (page1)');
-
-        fakeRequestPromise('yelp.com/api/coffee/page2')
-            .then(() => {        
-                console.log('IT WORKED!!! (page2)');
-            })
-            .catch(() => {
-                console.log('OH NO, A REQUEST ERROR!!! (page2)');
-            });
+        console.log(data);
+        return fakeRequestPromise('yelp.com/api/coffee/page2')
     })
-    .catch(() => {
-        console.log('OH NO, A REQUEST ERROR!!! (page1)');
+    .then(data => {
+        console.log('IT WORKED!!! (page2)');
+        console.log(data);
+    })
+    .catch(error => {
+        console.log('OH NO, A REQUEST ERROR!!!');
+        console.log(error);
     });
-
