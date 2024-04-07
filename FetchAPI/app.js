834f91858c20757e5f1c4d8c890c631711c191ee
정영일 <bbangwon@outlook.com>
2024-04-08 01:43:32 +0900

    Axios로 헤더 세팅하기

--- FetchAPI/app.js
+++ FetchAPI/app.js
@@ -16,7 +16,7 @@
 //         const res = await fetch("https://swapi.dev/api/people/1");
 //         const data = await res.json();
 //         console.log("Async Done", data);
-    
+
 //         const res2 = await fetch("https://swapi.dev/api/people/2");
 //         const data2 = await res2.json();
 //         console.log("Async Done", data2);
@@ -39,13 +39,44 @@ axios.get("https://swapi.dev/api/people/1")
     });
 
 const getStarWarsPeople = async id => {
-    try{
+    try {
         const res = await axios.get(`https://swapi.dev/api/people/${id}`);
         console.log("Axios Async Done", res.data);
     }
-    catch(e){
+    catch (e) {
         console.log("ERROR!", e);
     }
 };
 
 getStarWarsPeople(5);
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+   
+//Headers
+const getDaaJoke = async () => {
+    try {
+        const res = await axios.get("https://icanhazdadjoke.com/",
+            {
+                headers: {
+                    Accept: "application/json"
+                }
+            });
+
+        console.log("Joke", res.data.joke);
+        return res.data.joke;
+    }
+    catch (e) {
+        console.log("NO JOKES AVALIABLE!", e);
+    }
+}
+
+const addNewJoke = async () => {
+    const jokeText = await getDaaJoke();
+    const newLI = document.createElement("LI");
+    newLI.append(jokeText);
+    jokes.append(newLI);
+}
+
+button.addEventListener("click", addNewJoke);
+
