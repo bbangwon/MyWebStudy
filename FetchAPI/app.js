4dfb9ae16f0635b0cf9d8080790e582488d688f9
youngil <youngil@placeb.com>
2024-04-08 19:56:28 +0900

    TV 프로그램 검색 앱

--- FetchAPI/app.js
+++ FetchAPI/app.js
@@ -80,3 +80,29 @@ const addNewJoke = async () => {
 
 button.addEventListener("click", addNewJoke);
 
+
+// https://www.tvmaze.com/api
+
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+
+    console.log(res.data[0].show.image.medium);
+    makeImages(res.data);
+    
+    form.elements.query.value = "";
+});
+
+const makeImages = shows => {
+    for(let result of shows) {
+        if(result.show.image) {
+            const img = document.createElement("IMG");
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
