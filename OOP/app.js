3823443838b162d43abf10a6d2de969d7d4ebbf0
youngil <youngil@placeb.com>
2024-04-09 11:55:44 +0900

    class 키워드

--- OOP/app.js
+++ OOP/app.js
@@ -28,30 +28,61 @@ firstColor.rgb(); // 'rgb(35, 255, 150)'
 
 
 
-function Color(r, g, b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
+// function Color(r, g, b) {
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
 
-    this.rgb = function() {
+//     rgb = function() {
+//         const { r, g, b } = this;
+//         return `rgb(${r}, ${g}, ${b})`;
+//     };
+
+//     console.log(this);
+// }
+
+// Color.prototype.hex = function() {
+//     const { r, g, b } = this;
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// Color.prototype.rgba = function(a = 1.0) {
+//     const { r, g, b } = this;
+//     return `rgba(${r}, ${g}, ${b}, ${a})`;
+// }
+
+// const color1 = new Color(255, 40, 100); // Color {r: 255, g: 40, b: 100}
+// console.log(color1.rgb()); // rgb(255, 40, 100)
+// const color2 = new Color(0, 0, 0); // Color {r: 0, g: 0, b: 0}
+// console.log(color2.hex()); // #000000
+
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+
+    innerRGB() {
         const { r, g, b } = this;
-        return `rgb(${r}, ${g}, ${b})`;
-    };
+        return `${r}, ${g}, ${b}`;
+    }
 
-    console.log(this);
+    rgb() {
+        const { r, g, b } = this;
+        return `rgb(${this.innerRGB()})`;
+    }
+    
+    rgba(a = 1.0) {
+        const { r, g, b } = this;
+        return `rgba(${this.innerRGB()}, ${a})`;
+    }
+
+    hex() {
+        const { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);   
+    }
 }
 
-Color.prototype.hex = function() {
-    const { r, g, b } = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-Color.prototype.rgba = function(a = 1.0) {
-    const { r, g, b } = this;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
-}
-
-const color1 = new Color(255, 40, 100); // Color {r: 255, g: 40, b: 100}
-console.log(color1.rgb()); // rgb(255, 40, 100)
-const color2 = new Color(0, 0, 0); // Color {r: 0, g: 0, b: 0}
-console.log(color2.hex()); // #000000
+const c1 = new Color(255, 67, 89, 'tomato');
