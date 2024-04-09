7e47e9c7d855ff8f56b0a6f7762377b618504842
youngil <youngil@placeb.com>
2024-04-09 11:16:09 +0900

    생성자

--- OOP/app.js
+++ OOP/app.js
@@ -24,3 +24,34 @@ function makeColor(r, g, b) {
 
 const firstColor = makeColor(35, 255, 150);
 firstColor.rgb(); // 'rgb(35, 255, 150)'
+
+
+
+
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+
+    this.rgb = function() {
+        const { r, g, b } = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+
+    console.log(this);
+}
+
+Color.prototype.hex = function() {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+Color.prototype.rgba = function(a = 1.0) {
+    const { r, g, b } = this;
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+const color1 = new Color(255, 40, 100); // Color {r: 255, g: 40, b: 100}
+console.log(color1.rgb()); // rgb(255, 40, 100)
+const color2 = new Color(0, 0, 0); // Color {r: 0, g: 0, b: 0}
+console.log(color2.hex()); // #000000
