f421b11b9f5104e220776540d7b0ed2cd413d01f
정영일 <bbangwon@outlook.com>
2024-04-14 13:26:56 +0900

    require, module.exports

---
+++ NodeIntro/mathapp.js
@@ -0,0 +1,9 @@
+const math = require('./math');
+const { PI } = require('./math');
+console.log(math);
+
+console.log(math.PI);
+console.log(PI);
+
+// console.log(math.add(1, 2));
+// console.log(math.square(3));
