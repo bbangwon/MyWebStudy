6dc51ebee1edac389ed4149db5e693c53b813463
정영일 <bbangwon@outlook.com>
2024-04-17 02:02:48 +0900

    뷰 디렉토리 설정하기

--- EJS_DEMO/index.js
+++ EJS_DEMO/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import { fileURLToPath } from "url";
 
 const app = express();
 
 // EJS 템플릿 엔진 설정
 app.set('view engine', 'ejs');
+
+// EJS 파일이 있는 디렉토리 설정(index.js 파일의 디렉토리 경로를 기준으로 views 폴더를 설정)
+const viewsFolder = fileURLToPath(new URL("./views", import.meta.url));
+app.set('views', viewsFolder);
 
 app.get('/', (req, res) => {
     res.render('home.ejs');
