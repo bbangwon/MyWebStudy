3fe59f932969c9f3d19066eac8009b77aac8f4f4
youngil <youngil@placeb.com>
2024-04-17 20:07:54 +0900

    템플릿에 데이터 전달하기

--- EJS_DEMO/index.js
+++ EJS_DEMO/index.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
     res.render('home.ejs');
 });
 
+app.get('/rand', (req, res) =>{
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random.ejs', {num});
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
