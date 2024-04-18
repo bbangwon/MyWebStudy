9b21436ad47e001897edd7d31478802798d25eb7
youngil <youngil@placeb.com>
2024-04-18 09:10:23 +0900

    복잡한 서브레딧 데모

--- EJS_DEMO/index.js
+++ EJS_DEMO/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { fileURLToPath } from "url";
 
+// JSON 파일을 import 하기 위한 설정 (assert 옵션을 사용하여 JSON 파일임을 명시)
+import redditData from './data.json' assert {type: "json"};
+
 const app = express();
 
 // EJS 템플릿 엔진 설정
@@ -11,22 +14,32 @@ const viewsFolder = fileURLToPath(new URL("./views", import.meta.url));
 app.set('views', viewsFolder);
 
 app.get('/', (req, res) => {
-    res.render('home.ejs');
+    res.render('home');
 });
 
 app.get('/cats', (req, res) => {
     const cats = ['Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'];
-    res.render('cats.ejs', { cats });
+    res.render('cats', { cats });
 });
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    res.render('subreddit.ejs', { subreddit });
+    const data = redditData[subreddit];
+    if(data)
+    {
+        //객체를 펼쳐서 전달하면 EJS에서 객체의 속성을 변수로 사용할 수 있음
+        //예) data.name => name, data.description => description
+        res.render('subreddit', { ...data });
+    }
+    else
+    {
+        res.render('notfound', { subreddit });
+    }
 });
 
 app.get('/rand', (req, res) =>{
     const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random.ejs', {num});
+    res.render('random', {num});
 });
 
 app.listen(3000, () => {
