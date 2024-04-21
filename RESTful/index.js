1e97dc1bf114e33ab31d610b7839be3a5fdc5259
정영일 <bbangwon@outlook.com>
2024-04-21 13:58:26 +0900

    Update 페이지 - ES6에서 Method-override 를 지원하지 않아 fetch 메서드로 호출

--- RESTful/index.js
+++ RESTful/index.js
@@ -51,12 +51,18 @@ app.get('/comments/:id', (req, res) => {
     res.render('comments/show', { comment });
 });
 
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(comment => comment.id === id);
+    res.render('comments/edit', { comment });
+});
+
 app.patch('/comments/:id', (req, res) => {
     const { id } = req.params;
     const newCommentText = req.body.comment;    
     const comment = comments.find(comment => comment.id === id);
     comment.comment = newCommentText;
-    res.redirect('/comments');
+    res.send({ success: true });
 });
 
 
