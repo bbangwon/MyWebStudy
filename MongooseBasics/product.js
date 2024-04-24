94b542b6b9527a9ded32c20a0fb8a6095843bee1
정영일 <bbangwon@outlook.com>
2024-04-25 00:11:40 +0900

    추가 스키마 제약조건

--- MongooseBasics/product.js
+++ MongooseBasics/product.js
@@ -11,17 +11,42 @@ async function main() {
         name: {
             type: String,
             required: true,
+            maxlength: 20,
         },
         price: {
             type: Number,
             required: true,
+            min: 0,
         },
+        onSale: {
+            type: Boolean,
+            default: false,
+        },
+        categories: [String],
+        qty: {
+            online: {
+                type: Number,
+                default: 0,
+            },
+            inStore: {
+                type: Number,
+                default: 0,
+            }
+        }
     });
     const Product = mongoose.model('Product', productSchema);
 
     //스키마 유효성 검사
-    const bike = new Product({ name: 'Mountain Bike', price: '599', color: 'blue' });
+    const bike = new Product({
+        name: 'Bike Helmet', price: 19.50,
+        categories: ['Cycling', 'Safety'],
+        qty: {
+            online: 10,
+            inStore: 20
+        }
+    });
+
     bike.save()
         .then(data => console.log(data))
-        .catch(err => console.log(err));    
+        .catch(err => console.log(err));
 }
