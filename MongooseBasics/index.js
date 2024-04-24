09e0cc52135c63f2372e3dad73bcfdda5a556b26
youngil <youngil@placeb.com>
2024-04-24 14:02:21 +0900

    Mongoose로 찾기

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -21,17 +21,33 @@ async function main() {
     // await amadeus.save();
 
     //대량 데이터 추가
-    Movie.insertMany([
-        { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
-        { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
-        { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
-        { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
-        { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
-    ]);
+    // Movie.insertMany([
+    //     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    //     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    //     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    //     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    //     { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+    // ]);
 
-    
+    //데이터 조회
+    const movies = await Movie.find({});
+    console.log(movies);      
+    console.log('---------------------------------');
 
+    //첫번째 데이터 조회
+    const movie = await Movie.findOne({});
+    console.log(movie);
 
+    console.log('---------------------------------');
+
+    //조건에 맞는 데이터 조회
+    const movies2 = await Movie.find({ year: { $gte: 2000 } });
+    console.log(movies2);
+
+    console.log('---------------------------------');
+    //findById
+    const moviefromId = await Movie.findById('66288cb8ab56a2277ed23d26');
+    console.log(moviefromId);
 }
 
 
