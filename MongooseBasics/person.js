93638a0c9961a72161db5df8e77e69c3d48092bc
youngil <youngil@placeb.com>
2024-04-25 19:53:19 +0900

    가상 Mongoose

---
+++ MongooseBasics/person.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/shopApp');
+    console.log('Database connected');
+
+    //모델 생성
+    const personSchema = new mongoose.Schema({
+        first: String,
+        last: String
+    });
+
+    //가상 속성
+    personSchema.virtual('fullName').get(function() {
+        return `${this.first} ${this.last}`;
+    });
+
+    personSchema.virtual('fullName').set(function(v) {
+        this.first = v.substr(0, v.indexOf(' '));
+        this.last = v.substr(v.indexOf(' ') + 1);
+    });
+
+    const Person = mongoose.model('Person', personSchema);
+    const jordan = new Person({ first: 'Jordan', last: 'Hayashi' });
+
+    console.log(jordan.fullName);
+
+    jordan.fullName = 'Michael Jordan';
+    console.log(jordan.fullName);
+    console.log(jordan.first);
+    console.log(jordan.last);
+
+    
+}
