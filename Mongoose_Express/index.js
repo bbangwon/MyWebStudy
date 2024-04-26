5a822e0969424ccd6eed3c7042e194170299382f
youngil <youngil@placeb.com>
2024-04-26 20:54:25 +0900

    모델 만들기

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import { fileURLToPath } from "url";
 import mongoose from 'mongoose';
+import Product from './models/product.js';
 
 //Express
 const app = express();
+
+//Mongoose MongoDB 연결
+mongoose.connect('mongodb://localhost:27017/farmStand')
+.then(() => {
+    console.log('Database connected');
+})
+.catch(err => {
+    console.log('Error');
+    console.log(err);
+});
 
 const viewsFolder = fileURLToPath(new URL("./views", import.meta.url));
 app.set('views', viewsFolder);
@@ -19,15 +30,7 @@ app.listen(3000, () => {
 });
 
 
-//Mongoose MongoDB 연결
-mongoose.connect('mongodb://localhost:27017/movieApp')
-.then(() => {
-    console.log('Database connected');
-})
-.catch(err => {
-    console.log('Error');
-    console.log(err);
-});
+
 
 
 
