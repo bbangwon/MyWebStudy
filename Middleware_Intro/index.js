0f88b3f8c77417576e6389e6f7616bc58765c7a6
youngil <youngil@placeb.com>
2024-04-29 13:28:14 +0900

    미들웨어 연습 더 알아보기

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -4,21 +4,29 @@ import morgan from 'morgan';
 const app = express();
 
 app.use(morgan('tiny'));
-app.use((req, res, next) => {
-    console.log('Hello, I am a middleware');    
-    return next();
-});
 
 app.use((req, res, next) => {
-    console.log('Hello, I am another middleware');    
-    return next();
+    req.requestTime = Date.now();
+    next();
 });
+
+// app.use((req, res, next) => {
+//     console.log('Hello, I am a middleware');    
+//     return next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('Hello, I am another middleware');    
+//     return next();
+// });
 
 app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('Home Page');
 });
 
 app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('Woof Woof');
 });
 
