89febd7ab6ed92d3b4c2114418e61826f572835c
정영일 <bbangwon@outlook.com>
2024-05-01 00:45:49 +0900

    사용자 지정 오류 클래스

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import AppError from './AppError.js';
 
 const app = express();
 
@@ -20,7 +21,7 @@ const verfyPassword = ((req, res, next) => {
     if(password === 'chickennugget') {
         return next();
     }
-    throw new Error('Password required!');
+    throw new AppError(401, 'Password required!');
 });
 
 // app.use((req, res, next) => {
@@ -51,21 +52,32 @@ app.get('/secret', verfyPassword, (req, res) => {
     res.send('My secret is: Sometimes I wear headphones in public so I don\'t have to talk to anyone');
 });
 
+app.get('/admin', (req, res) => {
+    throw new AppError(403, 'You are not an admin!');
+});
+
 app.use((req, res) => {
     res.status(404).send('Not Found');
 })
 
+// //에러 핸들러
+// app.use((err, req, res, next) => {
+//     console.log('*******************************');
+//     console.log('**********ERROR HANDLER********');
+//     console.log('*******************************');
+//     console.log(err);
+
+//     //빌트인 에러핸들러 실행
+//     next(err);
+//     //res.status(500).send('Something went wrong');
+// });
+
 //에러 핸들러
 app.use((err, req, res, next) => {
-    console.log('*******************************');
-    console.log('**********ERROR HANDLER********');
-    console.log('*******************************');
-    console.log(err);
-
-    //빌트인 에러핸들러 실행
-    next(err);
-    //res.status(500).send('Something went wrong');
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message);
 });
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
