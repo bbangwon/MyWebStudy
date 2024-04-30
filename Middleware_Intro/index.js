beb88dc7caefe95a189bb1bee8b1c7a6558ad7fb
정영일 <bbangwon@outlook.com>
2024-05-01 00:15:38 +0900

    Express의 태생적 오류 처리기

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -20,7 +20,7 @@ const verfyPassword = ((req, res, next) => {
     if(password === 'chickennugget') {
         return next();
     }
-    res.send('Sorry you need a password!');    
+    throw new Error('Password required!');
 });
 
 // app.use((req, res, next) => {
@@ -36,6 +36,10 @@ const verfyPassword = ((req, res, next) => {
 app.get('/', (req, res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('Home Page');
+});
+
+app.get('/error', (req, res) => {
+    chicken.fly();
 });
 
 app.get('/dogs', (req, res) => {
