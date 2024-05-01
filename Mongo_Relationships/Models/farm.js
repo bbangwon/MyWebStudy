bc0641208a0cc656c261f165a26d606398725a77
youngil <youngil@placeb.com>
2024-05-02 08:56:16 +0900

    One to Many 관계

---
+++ Mongo_Relationships/Models/farm.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+//Mongoose MongoDB 연결
+mongoose.connect('mongodb://localhost:27017/relationshipDB')
+    .then(() => {
+        console.log('Database connected');
+    })
+    .catch(err => {
+        console.log('Error');
+        console.log(err);
+    });
+
+const productSchema = new Schema({
+    name: String,
+    price: Number,
+    season: {
+        type: String,
+        enum: ['Spring', 'Summer', 'Fall', 'Winter']
+    }
+});
+
+
+// Product.insertMany([
+//     { name: 'Goddess Melon', price: 4.99, season: 'Summer' },
+//     { name: 'Sugar Baby Watermelon', price: 4.99, season: 'Summer' },
+//     { name: 'Asparagus', price: 3.99, season: 'Spring' }
+// ])
+//     .then(res => {
+//         console.log(res);
+//     })
+//     .catch(e => {
+//         console.log(e);
+//     });
+
+const farmSchema = new Schema({
+    name: String,
+    city: String,
+    products: [{ type: Schema.Types.ObjectId, ref: 'Product' }]
+});
+
+const Product = mongoose.model('Product', productSchema);
+const Farm = mongoose.model('Farm', farmSchema);
+
+// const makeFarm = async () => {
+//     const farm = new Farm({ name: 'Full Belly Farms', city: 'Guinda, CA' });
+//     const melon = await Product.findOne({ name: 'Goddess Melon' });
+//     farm.products.push(melon);
+//     await farm.save();
+//     console.log(farm);
+// }
+
+// makeFarm();
+
+const addProduct = async () => {
+    const farm = await Farm.findOne({ name: 'Full Belly Farms' });
+    const watermelon = await Product.findOne({ name: 'Sugar Baby Watermelon' });
+    farm.products.push(watermelon);
+    await farm.save();
+    console.log(farm);
+}
+
+addProduct();
+
+
