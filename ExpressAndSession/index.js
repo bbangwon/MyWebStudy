da07c354a876cb6388f79dbe7b241b3105c73176
youngil <youngil@placeb.com>
2024-05-03 17:29:04 +0900

    Express Session Plus

--- ExpressAndSession/index.js
+++ ExpressAndSession/index.js
@@ -2,17 +2,30 @@ import express from 'express';
 import session from 'express-session';
 
 const app = express();
-app.use(session({ 
+
+const sessionOptions = { 
     secret : 'thisisnotagoodsecret',
     resave: false,
     saveUninitialized: false
-}));
+};
+
+app.use(session(sessionOptions));
 
 app.get('/viewcount', (req, res) => {
     req.session.count = req.session.count + 1 || 1;
     res.send(`You have viewed this page ${req.session.count} times`);
 });
 
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+});
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}`);
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
