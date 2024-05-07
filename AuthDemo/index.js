33e138aa7c5dafc84578aa7725937b82e0237411
youngil <youngil@placeb.com>
2024-05-07 14:09:07 +0900

    Auth Demo 등록

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -1,17 +1,44 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import mongoose from 'mongoose';
 import { fileURLToPath } from "url";
 import User from './models/user.js';
+import bcrypt from 'bcrypt';
 
 const app = express();
+
+//Mongoose MongoDB 연결
+mongoose.connect('mongodb://localhost:27017/authDemo')
+    .then(() => {
+        console.log('Database connected');
+    })
+    .catch(err => {
+        console.log('Error');
+        console.log(err);
+    });
+
 
 app.set('view engine', 'ejs');
 
 const viewsFolder = fileURLToPath(new URL("./views", import.meta.url));
 app.set('views', viewsFolder);
 
+app.use(urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('This is the home page');
+});
 
 app.get('/register', (req, res) => {
     res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;    
+
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({ username, password: hash });
+    await user.save();
+    res.redirect('/');
 });
     
 
